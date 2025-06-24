9fe65f3643c41ebc96a78d8964d934d4daa96da5
rhehdgus8831 <rhehdgus8831@naver.com>
2025-06-24 16:32:01 +0900

    app.js 모듈화 및 함수 실행

---
+++ JS/app.js
@@ -0,0 +1,13 @@
+import {quoteModal} from './stopwatch/quoteModal.js'; // 모달 JS
+import {stopwatch} from './stopwatch/stopwatch.js'; // 스톱워치 JS
+
+
+// 스톱워치
+stopwatch()
+
+
+
+// 명언 모달 열고 닫기
+quoteModal();
+
+
